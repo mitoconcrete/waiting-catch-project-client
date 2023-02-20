c858ba078c68d9b3daf6736ab89b3de12b336ff5
mitoconcrete <xogns98@gmail.com>
2023-02-21 04:22:30 +0900

    [feat] 로그인 실패시 가입페이지로 이동.

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -3,24 +3,35 @@ import { Modal } from "ant-design-vue";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_HOST;
 
-// 요청 인터셉터 추가
-axios.interceptors.request.use(
-  function (config) {
-    // 요청이 전달되기 전에 작업 수행
-    return config;
+const http = axios.create();
+// 응답 인터셉터 추가
+http.interceptors.response.use(
+  function (response) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거
+    // 응답 데이터가 있는 작업 수행
+    return response;
   },
   function (error) {
-    // 요청 오류가 있는 경우 작업 수행
-    console.log(error);
-    Modal.error({
-      title: "에러",
-      content: "??",
-    });
+    if (error.response && error.response.status) {
+      switch (error.response.status) {
+        // status code가 401인 경우 `logout`을 커밋하고 `/login` 페이지로 리다이렉트
+        case 400:
+          Modal.error({
+            title: "에러",
+            content: error.response.data.message,
+          });
+          break;
+        case 401:
+          store.commit("/logout");
+          router.push("/login").catch(() => {});
+          break;
+        default:
+          return Promise.reject(error);
+      }
+    }
     return Promise.reject(error);
   }
 );
-
-const http = axios.create();
 
 export const api = {
   default: {
